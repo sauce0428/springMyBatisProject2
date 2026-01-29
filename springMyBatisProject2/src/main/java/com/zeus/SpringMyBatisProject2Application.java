65a73c4ed5d82e32de46723ed722277eceacca08
KTH <sauce0428@gmail.com>
2026-01-29 15:44:03 +0900

    [생성]

---
+++ springMyBatisProject2/src/main/java/com/zeus/SpringMyBatisProject2Application.java
@@ -0,0 +1,13 @@
+package com.zeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyBatisProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMyBatisProject2Application.class, args);
+	}
+
+}
---
+++ springMyBatisProject2/src/test/java/com/zeus/SpringMyBatisProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMyBatisProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
